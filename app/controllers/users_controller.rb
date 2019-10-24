9157f51cb3b35d2e1f505d023df67dc93080338d
vagrant <vagrant@localhost.localdomain>
2019-10-24 07:37:14 +0000

    add loginform
    ログイン機能の追加　ロールごとには未対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user, {only: [:index]}
+    before_action :forbid_login_user, {only: [:login_form, :login]}
+
+    def login
+        @user = User.find_by(userid: params[:userid])
+        if @user && @user.authenticate(params[:password])
+            session[:userid] = @user.userid
+            flash[:notice] = "ログインしました"
+            redirect_to("/index")
+        else
+            flash[:notice] = "ユーザIDまたはパスワードが間違っています"
+            @userid = params[:userid]
+            @password = params[:password]
+            render("users/login_form")
+        end
+    end
+
+    def login_form
+        @user = User.new
+    end
+
+    def index
+
+    end
 end
