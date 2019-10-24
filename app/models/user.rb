dae6fbe2536a6b68167e074979a69d54431b5758
vagrant <vagrant@localhost.localdomain>
2019-10-24 07:23:49 +0000

    fix model
    各テーブルに対応したモデルの再生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :userid, {presence: true,uniqueness: true}
+    validates :password, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+
+  get "index" => "users#index"
+
+  get "/" => "users#login_form"
 end
